b7738ae40ac42f061736c9d8fc3fdef526aa62ae
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-03-31 02:31:58 +0900

    use xargs

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-for pkg in `./list-all-packages.sh`; do
-    ./upload-to-bintray.sh $pkg
-done
+# for pkg in `./list-all-packages.sh`; do
+#     ./upload-to-bintray.sh $pkg
+# done
+./list-all-packages.sh | xargs -i --max-procs=10 ./upload-to-bintray.sh {}
