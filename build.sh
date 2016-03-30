f89906f1f5271a775cdf71c5f7bc83a6e1ef2a75
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-03-31 02:41:08 +0900

    set upload concurrency to 4

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+: ${UPLOAD_CONCURRENCY:=4}
+
 # for pkg in `./list-all-packages.sh`; do
 #     ./upload-to-bintray.sh $pkg
 # done
-./list-all-packages.sh | xargs -i --max-procs=10 ./upload-to-bintray.sh {}
+./list-all-packages.sh | xargs -i --max-procs=$UPLOAD_CONCURRENCY ./upload-to-bintray.sh {}
